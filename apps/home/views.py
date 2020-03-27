9d21d2f192ffc6527499063891686707f54ed0c5
ldom <louis.giron@ensae-paristech.fr>
2020-03-27 22:47:43 +0100

    update home

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from assets.models import AssetsInfo
 
 
 def view_home(request):
+    assets_infos = AssetsInfo.objects.all().order_by('dividende', 'variation').reverse()
 
-    context = {}
+    context = {'assets_infos': assets_infos}
 
     return render(request, 'home/home.html', context)
