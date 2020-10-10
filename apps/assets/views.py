f4b5d4e398123b02643ba75c32d246e5f26aaef4
Louis Giron <louis.giron@engie.com>
2020-10-10 17:36:57 +0200

    add top 5 asset info

--- apps/assets/views.py
+++ apps/assets/views.py
@@ -7,3 +7,8 @@ from .models import AssetsInfo
 class AssetsInfoViewSet(viewsets.ModelViewSet):
     queryset = AssetsInfo.objects.all().order_by('-dividende', 'value')
     serializer_class = AssetsInfoSerializer
+
+
+class Top5AssetsInfoViewSet(viewsets.ModelViewSet):
+    queryset = AssetsInfo.objects.all().order_by('-dividende', 'value')[:5]
+    serializer_class = AssetsInfoSerializer
