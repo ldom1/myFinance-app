a0c877d9738370a48c2dd3bc0b2255ef828acc99
Louis Giron <louis.giron@engie.com>
2020-12-07 12:26:42 +0100

    decomment for deploy

--- apps/assets/urls.py
+++ apps/assets/urls.py
@@ -1,17 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import *
-'''
+
 router = routers.DefaultRouter()
 router.register(r'assets_info', AssetsInfoViewSet)
 router.register(r'optimal_assets_info', TopOptimAssetsInfoViewSet)
 router.register(r'optimal_assets_info_previously_selected', TopOptimAssetsInfoPreviouslySelectedViewSet)
 router.register(r'assets_check_limit', AssetsCheckLimitsViewSet)
-router.register(r'assets_check_limit_names', AssetsCheckLimitsAssetsNamesViewSet)'''
+router.register(r'assets_check_limit_names', AssetsCheckLimitsAssetsNamesViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    #path('', include(router.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- test_code_mngt.sh
+++ test_code_mngt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# heroku run python3 manage.py cleanAssetsDB
-# heroku run python3 manage.py getAssetsInfos
-# heroku run python3 manage.py getOptimalAssets
-# heroku run python3 manage.py getAndCheckAssetsLimits
+heroku run python3 manage.py cleanAssetsDB
+heroku run python3 manage.py getAssetsInfos
+heroku run python3 manage.py getOptimalAssets
+heroku run python3 manage.py getAndCheckAssetsLimits
 
-python3 manage.py cleanAssetsDB
-python3 manage.py getAssetsInfos
-python3 manage.py getOptimalAssets
-python3 manage.py getAndCheckAssetsLimits
+# python3 manage.py cleanAssetsDB
+# python3 manage.py getAssetsInfos
+# python3 manage.py getOptimalAssets
+# python3 manage.py getAndCheckAssetsLimits
