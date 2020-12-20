29c5b3cb888e5baa559605242afa31bf3f8bbf69
Louis Giron <louis.giron@engie.com>
2020-12-20 17:08:51 +0100

    handle exceptions on check limits

--- code/assets/get_and_manage_assets_limits.py
+++ code/assets/get_and_manage_assets_limits.py
@@ -14,38 +14,42 @@ def add_assets_info_to_assets_check_limit():
 
         logger.info(f"Add asset info in asset check limits: {asset_info.name} ({datetime.datetime.today()})")
 
-        id_asset = asset_info.id_asset
-        value_objective, potential_percent = get_asset_recommended_info(id_asset=id_asset)
+        try:
+            id_asset = asset_info.id_asset
+            value_objective, potential_percent = get_asset_recommended_info(id_asset=id_asset)
 
-        if AssetsCheckLimits.objects.filter(id_asset=asset_info.id_asset).exists():
-            logger.info(f"Add asset info in asset check limits: {asset_info.name} already exists")
-            response = AssetsCheckLimits.objects.update_or_create(
-                id_asset=id_asset,
-                defaults={'date': datetime.datetime.today(),
-                          'value': asset_info.value,
-                          'value_objective': value_objective,
-                          'potential_percent': potential_percent,
-                          }
-            )
-            logging.info(f'Add asset info in asset check limits - Update: {response}')
-        else:
-            AssetsCheckLimits.objects.get_or_create(
-                date=datetime.datetime.today(),
-                date_update=asset_info.date_update,
-                id_asset=id_asset,
-                name=asset_info.name,
-                symbol=asset_info.symbol,
-                longname=asset_info.longname,
-                down_limit=None,
-                down_limit_knocked=False,
-                up_limit=None,
-                up_limit_knocked=False,
-                url=asset_info.url,
-                value=asset_info.value,
-                value_objective=value_objective,
-                potential_percent=potential_percent,
-            )
-        logger.info(f'Add asset info in asset check limits: {asset_info.name} added in db')
+            if AssetsCheckLimits.objects.filter(id_asset=asset_info.id_asset).exists():
+                logger.info(f"Add asset info in asset check limits: {asset_info.name} already exists")
+                response = AssetsCheckLimits.objects.update_or_create(
+                    id_asset=id_asset,
+                    defaults={'date': datetime.datetime.today(),
+                              'value': asset_info.value,
+                              'value_objective': value_objective,
+                              'potential_percent': potential_percent,
+                              }
+                )
+                logging.info(f'Add asset info in asset check limits - Update: {response}')
+            else:
+                AssetsCheckLimits.objects.get_or_create(
+                    date=datetime.datetime.today(),
+                    date_update=asset_info.date_update,
+                    id_asset=id_asset,
+                    name=asset_info.name,
+                    symbol=asset_info.symbol,
+                    longname=asset_info.longname,
+                    down_limit=None,
+                    down_limit_knocked=False,
+                    up_limit=None,
+                    up_limit_knocked=False,
+                    url=asset_info.url,
+                    value=asset_info.value,
+                    value_objective=value_objective,
+                    potential_percent=potential_percent,
+                )
+            logger.info(f'Add asset info in asset check limits: {asset_info.name} added in db')
+
+        except Exception as e:
+            logging.info(f'Add asset info in asset check limits - ERROR: {e}')
 
 
 def get_asset_value(symbol):
@@ -70,13 +74,16 @@ def update_up_limit():
 
         logger.info(f"Get and check asset limits: update up limit: {asset.longname}")
 
-        asset_value = get_asset_value(asset.symbol)
+        try:
+            asset_value = get_asset_value(asset.symbol)
 
-        if asset_value >= asset.up_limit:
-            logger.info(
-                f"Get and check asset limits: up limit knocked: current value: {asset_value} - limit {asset.up_limit}")
-            asset.up_limit_knocked = True
-            asset.save()
+            if asset_value >= asset.up_limit:
+                logger.info(
+                    f"Get and check asset limits: up limit knocked: current value: {asset_value} - limit {asset.up_limit}")
+                asset.up_limit_knocked = True
+                asset.save()
+        except Exception as e:
+            logger.info(f"Get and check asset limits: update up limit: ERROR {e}")
 
 
 def update_down_limit():
@@ -86,13 +93,16 @@ def update_down_limit():
 
         logger.info(f"Get and check asset limits: update down limit: {asset.longname}")
 
-        asset_value = get_asset_value(asset.symbol)
+        try:
+            asset_value = get_asset_value(asset.symbol)
 
-        if asset_value <= asset.down_limit:
-            logger.info(
-                f"Get and check asset limits: down limit knocked: current value: {asset_value} - limit {asset.down_limit}")
-            asset.down_limit_knocked = True
-            asset.save()
+            if asset_value <= asset.down_limit:
+                logger.info(
+                    f"Get and check asset limits: down limit knocked: current value: {asset_value} - limit {asset.down_limit}")
+                asset.down_limit_knocked = True
+                asset.save()
+        except Exception as e:
+            logger.info(f"Get and check asset limits: update down limit: ERROR {e}")
 
 
 def update_limits():
