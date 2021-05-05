ffd0c11ba83ffac7b29b4dde1fe9c9e53834663a
ldom <louis.giron@ensae-paristech.fr>
2021-05-05 09:50:52 +0200

    migrate

--- apps/assets/views.py
+++ apps/assets/views.py
@@ -11,35 +11,40 @@ class AssetsInfoViewSet(viewsets.ModelViewSet):
 
 
 class TopOptimAssetsInfoViewSet(viewsets.ModelViewSet):
+    """
     previously_selected = False
     last_date = OptimAssetsInfo.objects.filter(previously_selected=previously_selected).latest(
         'date_update').date_update
     queryset = OptimAssetsInfo.objects.filter(date_update=last_date).filter(
         previously_selected=previously_selected).exclude(id_asset__isnull=True).order_by('-weight')
-    serializer_class = OptimAssetsInfoSerializer
+    serializer_class = OptimAssetsInfoSerializer"""
 
 
 class TopOptimAssetsInfoPreviouslySelectedViewSet(viewsets.ModelViewSet):
+    """
     previously_selected = True
     last_date = OptimAssetsInfo.objects.filter(previously_selected=previously_selected).latest(
         'date_update').date_update
     queryset = OptimAssetsInfo.objects.filter(date_update=last_date).filter(
         previously_selected=previously_selected).exclude(id_asset__isnull=True).order_by('-weight')
-    serializer_class = OptimAssetsInfoSerializer
+    serializer_class = OptimAssetsInfoSerializer"""
 
 
 class AssetsCheckLimitsViewSet(viewsets.ModelViewSet):
+    """
     queryset = AssetsCheckLimits.objects.exclude(
         Q(up_limit__isnull=True) & Q(down_limit__isnull=True)).order_by('name')
-    serializer_class = AssetsLimitCheckerSerializer
+    serializer_class = AssetsLimitCheckerSerializer"""
 
 
 class AssetsCheckLimitsAssetsNamesViewSet(viewsets.ModelViewSet):
+    """
     queryset = AssetsCheckLimits.objects.exclude(name__isnull=True).order_by('name')
-    serializer_class = AssetsLimitCheckerSerializer
+    serializer_class = AssetsLimitCheckerSerializer"""
 
 
 class RecommendedAssetsViewSet(viewsets.ModelViewSet):
+    """
     last_date = RecommendedAssetsToBuy.objects.latest('date_date').date_date
     queryset = RecommendedAssetsToBuy.objects.filter(date_date=last_date).order_by('-potential_percent')
-    serializer_class = RecommendedAssetsSerializer
+    serializer_class = RecommendedAssetsSerializer"""
