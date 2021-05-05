0bad248cb34c7c12ccfb0da4c35be46de31a436b
ldom <louis.giron@ensae-paristech.fr>
2021-05-05 09:24:41 +0200

    migrate

--- apps/pea/forms.py
+++ apps/pea/forms.py
@@ -2,56 +2,56 @@ from django import forms
 from django.apps import apps
 import numpy as np
 
+
 class RegisterOrderFundsForm(forms.Form):
+    """
+    PEA = apps.get_model('pea', 'PEA')
+    fundsCA = apps.get_model('funds_CA', 'fundsCA')
 
-	PEA = apps.get_model('pea', 'PEA')
-	fundsCA = apps.get_model('funds_CA', 'fundsCA')
+    pea = PEA.objects.all()
+    funds = fundsCA.objects.all()
 
-	pea = PEA.objects.all()
-	funds = fundsCA.objects.all()
+    unique_pea = np.unique([y.name_pea for y in pea])
+    unique_funds = np.unique([y.name for y in funds])
 
-	unique_pea = np.unique([y.name_pea for y in pea])
-	unique_funds = np.unique([y.name for y in funds])
+    choices_curr = [[1, 'Choose...']] + [['EUR', 'EUR']] + [['USD', 'USD']]
+    choices_pea = [[1, 'Choose...']] + [[y, y] for y in unique_pea]
+    choices_asset = [[1, 'Choose...']] + [[y, y] for y in unique_funds]
+    choices_type_asset = [[1, 'Choose...']] + [['fond', 'fond']] + [['action', 'action']]
 
-	choices_curr = [[1, 'Choose...']] + [['EUR', 'EUR']] + [['USD', 'USD']]
-	choices_pea = [[1, 'Choose...']] + [[y, y] for y in unique_pea]
-	choices_asset = [[1, 'Choose...']] + [[y, y] for y in unique_funds]
-	choices_type_asset = [[1, 'Choose...']] + [['fond', 'fond']] + [['action', 'action']]
-
-	name = forms.ChoiceField(choices=choices_asset, required=True)
-	type_asset = forms.ChoiceField(choices=choices_type_asset, required=True)
-	initial_amount = forms.IntegerField(required=True)
-	name_pea = forms.ChoiceField(choices=choices_pea, required=True)
-	currency = forms.ChoiceField(choices=choices_curr, required=True)
+    name = forms.ChoiceField(choices=choices_asset, required=True)
+    type_asset = forms.ChoiceField(choices=choices_type_asset, required=True)
+    initial_amount = forms.IntegerField(required=True)
+    name_pea = forms.ChoiceField(choices=choices_pea, required=True)
+    currency = forms.ChoiceField(choices=choices_curr, required=True)"""
 
 
 class RegisterOrderAssetForm(forms.Form):
+    """
+    PEA = apps.get_model('pea', 'PEA')
+    assets = apps.get_model('assets', 'Assets')
 
-	PEA = apps.get_model('pea', 'PEA')
-	assets = apps.get_model('assets', 'Assets')
+    pea = PEA.objects.all()
+    asset = assets.objects.all()
 
-	pea = PEA.objects.all()
-	asset = assets.objects.all()
+    unique_pea = np.unique([y.name_pea for y in pea])
+    unique_asset = np.unique([y.name for y in asset])
 
-	unique_pea = np.unique([y.name_pea for y in pea])
-	unique_asset = np.unique([y.name for y in asset])
+    choices_curr = [[1, 'Choose...']] + [['EUR', 'EUR']] + [['USD', 'USD']]
+    choices_pea = [[1, 'Choose...']] + [[y, y] for y in unique_pea]
+    choices_asset = [[1, 'Choose...']] + [[y, y] for y in unique_asset]
+    choices_type_asset = [[1, 'Choose...']] + [['fond', 'fond']] + [['action', 'action']]
 
-	choices_curr = [[1, 'Choose...']] + [['EUR', 'EUR']] + [['USD', 'USD']]
-	choices_pea = [[1, 'Choose...']] + [[y, y] for y in unique_pea]
-	choices_asset = [[1, 'Choose...']] + [[y, y] for y in unique_asset]
-	choices_type_asset = [[1, 'Choose...']] + [['fond', 'fond']] + [['action', 'action']]
-
-	name = forms.ChoiceField(choices=choices_asset, required=True)
-	type_asset = forms.ChoiceField(choices=choices_type_asset, required=True)
-	initial_amount = forms.IntegerField(required=True)
-	name_pea = forms.ChoiceField(choices=choices_pea, required=True)
-	currency = forms.ChoiceField(choices=choices_curr, required=True)
+    name = forms.ChoiceField(choices=choices_asset, required=True)
+    type_asset = forms.ChoiceField(choices=choices_type_asset, required=True)
+    initial_amount = forms.IntegerField(required=True)
+    name_pea = forms.ChoiceField(choices=choices_pea, required=True)
+    currency = forms.ChoiceField(choices=choices_curr, required=True)"""
 
 
 class RegisterPeaForm(forms.Form):
+    choices_curr = [[1, 'Choose...']] + [['EUR', 'EUR']] + [['USD', 'USD']]
 
-	choices_curr = [[1, 'Choose...']] + [['EUR', 'EUR']] + [['USD', 'USD']]
-
-	currency = forms.ChoiceField(choices=choices_curr, required=True)
-	name_pea = forms.CharField(required=True)
-	description = forms.CharField(required=True)
+    currency = forms.ChoiceField(choices=choices_curr, required=True)
+    name_pea = forms.CharField(required=True)
+    description = forms.CharField(required=True)
--- apps/pea/views.py
+++ apps/pea/views.py
@@ -12,218 +12,216 @@ from funds_CA.models import fundsCA
 from .forms import *
 from .models import *
 
+
 @login_required
 def view_display_pea(request):
+    id_pea = request.GET.get('get_id')
 
-	id_pea = request.GET.get('get_id')
+    if id_pea:
 
-	if id_pea:
+        pea_to_delete = PEA.objects.filter(id_pea=id_pea, user_username=request.user.get_username())
+        for pea in pea_to_delete:
+            pea.delete()
 
-		pea_to_delete = PEA.objects.filter(id_pea=id_pea, user_username=request.user.get_username())
-		for pea in pea_to_delete:
-			pea.delete()
+    pea = PEA.objects.filter(user_username=request.user.get_username())
 
-	pea = PEA.objects.filter(user_username=request.user.get_username())
+    if len(pea) == 0:
+        context = {}
+    else:
+        context = {'pea': pea}
 
-	if len(pea) == 0:
-		context = {}
-	else:
-		context = {'pea': pea}
-
-	return render(request, 'pea/display_pea.html', context)
-
+    return render(request, 'pea/display_pea.html', context)
 
 
 @login_required
 def view_display_one_pea(request, name_pea):
+    pea = PEA.objects.filter(name_pea=name_pea, user_username=request.user.get_username())[0]
+    pea_history = PEAHistory.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
+    orders = Order.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
 
-	pea = PEA.objects.filter(name_pea=name_pea, user_username=request.user.get_username())[0]
-	pea_history = PEAHistory.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
-	orders = Order.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
+    nb_orders = len(orders)
 
-	nb_orders = len(orders)
+    # Var for chart
+    risk = np.zeros(8)
 
-	# Var for chart
-	risk = np.zeros(8)
+    for order in orders:
+        try:
+            risk[order.risk - 1] += 1
+        except Exception:
+            pass
 
-	for order in orders:
-		try:
-			risk[order.risk-1] += 1
-		except Exception:
-			pass
+    context = {'pea': pea,
+               'pea_history': pea_history,
+               'orders': orders,
+               'nb_orders': nb_orders,
+               'risk': risk}
 
-	context = {'pea': pea,
-			   'pea_history': pea_history,
-			   'orders': orders,
-			   'nb_orders': nb_orders,
-			   'risk': risk}
+    return render(request, 'pea/display_one_pea.html', context)
 
-	return render(request, 'pea/display_one_pea.html', context)
 
 class view_register_new_pea(TemplateView):
-	# Form to get data
+    # Form to get data
     template_name = 'pea/register_new_pea.html'
 
     def get(self, request):
-    	context = {}
+        context = {}
 
-    	form = RegisterPeaForm()
-    	# context
-    	context['form'] = form
-    	return render(request, self.template_name, context)
+        form = RegisterPeaForm()
+        # context
+        context['form'] = form
+        return render(request, self.template_name, context)
 
     def post(self, request):
-    	context = {}
-    	form = RegisterPeaForm(request.POST)
+        context = {}
+        form = RegisterPeaForm(request.POST)
 
-    	if form.is_valid():
+        if form.is_valid():
 
-    		name_pea = form.cleaned_data['name_pea']
-    		description = form.cleaned_data['description']
-    		currency = form.cleaned_data['currency']
+            name_pea = form.cleaned_data['name_pea']
+            description = form.cleaned_data['description']
+            currency = form.cleaned_data['currency']
 
-    		# Get the pea
-    		pea = PEA.objects.filter(user_username=request.user.get_username())
+            # Get the pea
+            pea = PEA.objects.filter(user_username=request.user.get_username())
 
-    		try:
-    			id_pea = np.int(np.max([y.id_pea for y in pea]) + 1)
-    		except Exception:
-    			id_pea = 1
+            try:
+                id_pea = np.int(np.max([y.id_pea for y in pea]) + 1)
+            except Exception:
+                id_pea = 1
 
-
-    		pea, created = PEA.objects.get_or_create(
-				            	date = datetime.today(),
-				            	id_pea = id_pea,
-				            	name_pea = name_pea,
-				            	description = description,
-				            	current_value = 0,
-				            	currency = currency,
-				            	risk = 0,
-    							user_username = request.user.get_username())
-    	# context
-    	context['form'] = form
-    	return render(request, self.template_name, context)
+            pea, created = PEA.objects.get_or_create(
+                date=datetime.today(),
+                id_pea=id_pea,
+                name_pea=name_pea,
+                description=description,
+                current_value=0,
+                currency=currency,
+                risk=0,
+                user_username=request.user.get_username())
+        # context
+        context['form'] = form
+        return render(request, self.template_name, context)
 
 
 class view_register_new_order_funds(TemplateView):
-	# Form to get data
+    # Form to get data
     template_name = 'pea/register_new_order_funds.html'
 
     def get(self, request):
-    	context = {}
+        context = {}
 
-    	form = RegisterOrderFundsForm()
+        form = RegisterOrderFundsForm()
 
-    	# context
-    	context['form'] = form
+        # context
+        context['form'] = form
 
-    	return render(request, self.template_name, context)
+        return render(request, self.template_name, context)
 
     def post(self, request):
-    	context = {}
+        context = {}
 
-    	form = RegisterOrderFundsForm(request.POST)
+        form = RegisterOrderFundsForm(request.POST)
 
-    	if form.is_valid():
-    		type_asset = form.cleaned_data['type_asset']
-	    	name = form.cleaned_data['name']
-	    	initial_amount = form.cleaned_data['initial_amount']
-	    	currency = form.cleaned_data['currency']
-	    	name_pea = form.cleaned_data['name_pea']
+        if form.is_valid():
+            type_asset = form.cleaned_data['type_asset']
+            name = form.cleaned_data['name']
+            initial_amount = form.cleaned_data['initial_amount']
+            currency = form.cleaned_data['currency']
+            name_pea = form.cleaned_data['name_pea']
 
-	    	# Get the pea
-	    	pea = PEA.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
-	    	order = Order.objects.filter(user_username=request.user.get_username())
-	    	id_pea = pea[0].id_pea
+            # Get the pea
+            pea = PEA.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
+            order = Order.objects.filter(user_username=request.user.get_username())
+            id_pea = pea[0].id_pea
 
-	    	try:
-	    		id_order = np.int(np.max([y.id_order for y in order]) + 1)
-	    	except Exception:
-	    		id_order = 1
+            try:
+                id_order = np.int(np.max([y.id_order for y in order]) + 1)
+            except Exception:
+                id_order = 1
 
-	    	fundsCA = apps.get_model('funds_CA', 'fundsCA')
-    		funds = fundsCA.objects.all()
+            fundsCA = apps.get_model('funds_CA', 'fundsCA')
+            funds = fundsCA.objects.all()
 
-	    	risk = funds.filter(name=name)[0].risk_level
-	    	id_asset = funds.filter(name=name)[0].id_fund
+            risk = funds.filter(name=name)[0].risk_level
+            id_asset = funds.filter(name=name)[0].id_fund
 
-	    	order, created = Order.objects.get_or_create(
-				            	buying_date = datetime.today(),
-				            	type_asset=type_asset,
-				            	name_asset=name,
-				            	id_pea = id_pea,
-				            	id_order = id_order,
-				            	name_pea = name_pea,
-				            	id_asset = id_asset,
-				            	initial_amount = initial_amount,
-				            	current_value = initial_amount,
-				            	currency = currency,
-				            	live=1,
-				            	risk=risk,
-				            	user_username = request.user.get_username())
+            order, created = Order.objects.get_or_create(
+                buying_date=datetime.today(),
+                type_asset=type_asset,
+                name_asset=name,
+                id_pea=id_pea,
+                id_order=id_order,
+                name_pea=name_pea,
+                id_asset=id_asset,
+                initial_amount=initial_amount,
+                current_value=initial_amount,
+                currency=currency,
+                live=1,
+                risk=risk,
+                user_username=request.user.get_username())
 
-    	# context
-    	context['form'] = form
+        # context
+        context['form'] = form
 
-    	return render(request, self.template_name, context)
+        return render(request, self.template_name, context)
 
 
 class view_register_new_order_asset(TemplateView):
-	# Form to get data
+    # Form to get data
     template_name = 'pea/register_new_order_asset.html'
 
     def get(self, request):
-    	context = {}
+        context = {}
 
-    	form = RegisterOrderAssetForm()
+        form = RegisterOrderAssetForm()
 
-    	# context
-    	context['form'] = form
-    	return render(request, self.template_name, context)
+        # context
+        context['form'] = form
+        return render(request, self.template_name, context)
 
     def post(self, request):
-    	context = {}
+        context = {}
 
-    	form = RegisterOrderAssetForm(request.POST)
+        form = RegisterOrderAssetForm(request.POST)
 
-    	if form.is_valid():
-    		type_asset = form.cleaned_data['type_asset']
-	    	name = form.cleaned_data['name']
-	    	initial_amount = form.cleaned_data['initial_amount']
-	    	currency = form.cleaned_data['currency']
-	    	name_pea = form.cleaned_data['name_pea']
+        if form.is_valid():
+            type_asset = form.cleaned_data['type_asset']
+            name = form.cleaned_data['name']
+            initial_amount = form.cleaned_data['initial_amount']
+            currency = form.cleaned_data['currency']
+            name_pea = form.cleaned_data['name_pea']
 
-	    	# Get the pea
-	    	pea = PEA.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
-	    	order = Order.objects.filter(user_username=request.user.get_username())
-	    	id_pea = pea[0].id_pea
+            # Get the pea
+            pea = PEA.objects.filter(name_pea=name_pea, user_username=request.user.get_username())
+            order = Order.objects.filter(user_username=request.user.get_username())
+            id_pea = pea[0].id_pea
 
-	    	try:
-	    		id_order = np.int(np.max([y.id_order for y in order]) + 1)
-	    	except Exception:
-	    		id_order = 1
+            try:
+                id_order = np.int(np.max([y.id_order for y in order]) + 1)
+            except Exception:
+                id_order = 1
 
-	    	assets = apps.get_model('assets', 'Assets')
-    		asset = assets.objects.all()
+            assets = apps.get_model('assets', 'Assets')
+            asset = assets.objects.all()
 
-	    	id_asset = asset.filter(name=name)[0].id_asset
+            id_asset = asset.filter(name=name)[0].id_asset
 
-	    	order, created = Order.objects.get_or_create(
-				            	buying_date = datetime.today(),
-				            	type_asset=type_asset,
-				            	name_asset=name,
-				            	id_pea = id_pea,
-				            	id_order = id_order,
-				            	name_pea = name_pea,
-				            	id_asset = id_asset,
-				            	initial_amount = initial_amount,
-				            	current_value = initial_amount,
-				            	currency = currency,
-				            	live=1,
-				            	risk=8,
-				            	user_username = request.user.get_username())
+            order, created = Order.objects.get_or_create(
+                buying_date=datetime.today(),
+                type_asset=type_asset,
+                name_asset=name,
+                id_pea=id_pea,
+                id_order=id_order,
+                name_pea=name_pea,
+                id_asset=id_asset,
+                initial_amount=initial_amount,
+                current_value=initial_amount,
+                currency=currency,
+                live=1,
+                risk=8,
+                user_username=request.user.get_username())
 
-    	# context
-    	context['form'] = form
+        # context
+        context['form'] = form
 
-    	return render(request, self.template_name, context)
+        return render(request, self.template_name, context)
